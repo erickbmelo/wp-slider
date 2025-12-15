d7e5c03ca5c05715755228a7d55caf0c1325fa99
Erick Brito <erickbmelo@gmail.com>
2025-12-15 20:33:49 -0300

    Meta tags

--- mv-slider.php
+++ mv-slider.php
@@ -1,1 +1,30 @@
 <?php 
+
+/**
+ * Plugin Name: WP Slider
+ * Plugin URI: https://domain.com
+ * Description: Plugin's description
+ * Version: 1.0.0
+ * Requires at least: 5.6
+ * Author: Erick
+ * Author URI: https://domain2.com
+ * License: GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain: wp-slider
+ * Domain Path: /languages
+ */
+
+/*
+WP Slider is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+
+WP Slider is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with WP Slider. If not, see https://www.gnu.org/licenses/gpl-3.0.html.
+*/
